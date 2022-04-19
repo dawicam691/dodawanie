a2eb144caadf6db9adbc6343f59ee485bcadcc16
dawicam691 <dawicam691@student.polsl.pl>
2022-04-19 21:44:41 +0200

    commit3

--- src/test/java/DodawanieApplicationTest.java
+++ src/test/java/DodawanieApplicationTest.java
@@ -3,9 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DodawanieApplicationTest {
+
     @Test
-    public void added(){
-        DodawanieApplication dodawanieApplication = new DodawanieApplication();
+    public void added() {
+        DodawanieApplication dodawanieApplication=new DodawanieApplication();
         assertEquals(25, dodawanieApplication.added(5,5));
     }
 }
